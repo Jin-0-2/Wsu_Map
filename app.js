d1741c5a195ad6c7f0433e8efc18b166251b4c97
EC2 Default User <ec2-user@ip-172-31-1-33.ap-southeast-2.compute.internal>
2025-06-23 08:11:21 +0000

    db연결까지 성공

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const helloRouter = require('./routes/hello');
 const byeRouter = require('./routes/bye');
 
+// DB연결
+const db = require('./db');
+
 app.use(express.json());
 app.use('/hello', helloRouter);
 app.use('/bye', byeRouter);
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
