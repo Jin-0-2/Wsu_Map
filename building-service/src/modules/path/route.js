9a4ddfa48057a7259c391b4fb978a998d75ef73c
정진영 <jjy020815@gmail.com>
2025-06-30 16:07:53 +0900

    post로 바꿈

--- building-service/src/modules/path/route.js
+++ building-service/src/modules/path/route.js
@@ -5,6 +5,6 @@ const router = express.Router()
 const controller = require("./controller")
 
 // 경로 조회 및 반환
-router.get("/", controller.getPath)
+router.post("/", controller.getPath)
 
 module.exports = router
