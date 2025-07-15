208bfa402f920b91b9a79519d4ce3aea16508fb4
정진영 <jjy020815@gmail.com>
2025-07-15 11:23:13 +0900

    바꿔\봄

--- user-service/src/modules/timetable/controller.js
+++ user-service/src/modules/timetable/controller.js
@@ -4,3 +4,70 @@ const Service = require("./service")
 // const nodemailer = require('nodemailer');
 const { logRequestInfo } = require('../../core/logger'); // 경로는 상황에 맞게
 
+// 내 시간표 불러오기
+exports.getAll = async (req, res) => {
+  try {
+    logRequestInfo(req);
+
+    const id  = req.params.id;
+    
+    const result = await Service.getAll(id);
+    
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("DB 오류:", err);
+    
+    res.status(500).send("DB 오류");
+  }
+};
+
+// 시간표 추가
+exports.add = async (req, res) => {
+  try {
+    logRequestInfo(req);
+
+    const id  = req.params.id;
+    
+    const result = await Service.add(id);
+    
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("DB 오류:", err);
+    
+    res.status(500).send("DB 오류");
+  }
+};
+
+// 시간표 추가
+exports.update = async (req, res) => {
+  try {
+    logRequestInfo(req);
+
+    const id  = req.params.id;
+    
+    const result = await Service.update(id);
+    
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("DB 오류:", err);
+    
+    res.status(500).send("DB 오류");
+  }
+};
+
+// 시간표 삭제
+exports.delete = async (req, res) => {
+  try {
+    logRequestInfo(req);
+
+    const id  = req.params.id;
+    
+    const result = await Service.delete(id);
+    
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error("DB 오류:", err);
+    
+    res.status(500).send("DB 오류");
+  }
+};
