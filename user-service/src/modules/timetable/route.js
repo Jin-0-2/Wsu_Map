5d956b0d737b3e723a8c576c2b7735e8844ad204
정진영 <jjy020815@gmail.com>
2025-07-15 11:12:47 +0900

    love yourself

--- user-service/src/modules/timetable/route.js
+++ user-service/src/modules/timetable/route.js
@@ -4,6 +4,17 @@ const express = require("express")
 const router = express.Router()
 const controller = require("./controller")
 
+// 내 시간표 불러오기
+router.get("/:id", controller.getAll)
+
+// 시간표 추가
+router.post("/:id", controller.add)
+
+// 시간표 수정
+router.put("/:id", controller.update)
+
+// 사간표 삭제
+router.delete("/:id", controller.delete);
 
 
 module.exports = router
