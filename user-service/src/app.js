787211c7c49043542a0f2f61a55aea562f141375
정진영 <jjy020815@gmail.com>
2025-07-17 21:33:12 +0900

    미친 세션 등록

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -2,27 +2,35 @@
 
 const express = require('express');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+
 const app = express();
 const userRouter = require('./routes/index');
 const con = require('./core/db')
 
-const AWS_IP = `http://16.176.161.244:`
+const IP = `54.252.240.31`;
+const AWS_IP = `http://${IP}:`
+
 
 app.use(express.json());
 
 // ⭐️ 세션 등록은 라우터 등록(=app.use("/")) "이전"에! ⭐️
 app.use(session({
+  store: new RedisStore({ host: 'redis.address', port: 6379 }),
   secret: 'YJB_20250711',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 30 * 60 * 1000 } // 30분
+  cookie: { 
+    domain: IP,
+    path: '/',
+    maxAge: 30 * 60 * 1000 } // 30분
 }));
 
 app.use("/", userRouter);
 
 // ▼ 자동 로그아웃 배치 (필요시)
 setInterval(async () => {
-  const N_MIN = 10; // 10분
+  const N_MIN = 1; // 10분
   const sql = `
     UPDATE "User"
     SET "Is_Login" = false
@@ -30,7 +38,7 @@ setInterval(async () => {
     AND "Last_Location_Time" < NOW() - INTERVAL '${N_MIN} minutes'
   `;
   con.query(sql);
-}, 10 * 60 * 1000);
+}, 5 * 60 * 1000);
 
 const PORT = 3001;
 app.listen(PORT, '0.0.0.0', () => {
