639dced6a605b0afe4e0a6f36e1f60887f8c0c62
정진영 <jjy020815@gmail.com>
2025-07-18 13:58:36 +0900

    일단 유저 로그인 유무 껏다 키는거 성공

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -12,28 +12,6 @@ app.use("/", userRouter);
 
 const PORT = 3001;
 
-setInterval(async () => {
-  const N_MIN = 1; // 1분
-  const sql = `
-UPDATE "User"
-SET "Is_Login" = false
-WHERE "Is_Login" = true
-AND "Last_Location_Time" < NOW() - INTERVAL '${N_MIN} minutes'
-RETURNING "Name"
-`;
-  try {
-    const res = await con.query(sql);
-    const updatedNames = res.rows.map(row => row.Name);
-    if (updatedNames.length > 0) {
-      console.log('로그아웃 처리된 유저 이름들:', updatedNames);
-    }
-    else
-      console.log('로그아웃 처리된 유저가 없습니다');
-  } catch (err) {
-console.error('로그아웃 쿼리 실패:', err);
-  }
-}, 5 * 60 * 1000);
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on ${AWS_IP}${PORT}`);
 });
