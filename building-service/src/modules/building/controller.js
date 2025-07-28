68cbf125938bf4105e5c65ccb1d6e565c31cf971
정진영 <jjy020815@gmail.com>
2025-07-28 15:24:01 +0900

    콘솔 로그 찍어봄

--- building-service/src/modules/building/controller.js
+++ building-service/src/modules/building/controller.js
@@ -9,6 +9,8 @@ exports.getAll = async (req, res) => {
   try {
     const result = await Service.getAll();
 
+    console.log(result.rows);
+
     res.status(200).json(result.rows);
   } catch (err) {
     console.error("DB 오류:", err);
