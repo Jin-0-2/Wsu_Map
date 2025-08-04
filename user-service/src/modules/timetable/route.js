e9bff6867d2e1efe435341e8dcec38221d7af0a9
정진영 <jjy020815@gmail.com>
2025-08-04 21:03:51 +0900

    ㅈㅈ

--- user-service/src/modules/timetable/route.js
+++ user-service/src/modules/timetable/route.js
@@ -7,11 +7,11 @@ const controller = require("./controller")
 // 내 시간표 불러오기
 router.get("/:id", controller.getAll)
 
+// 시간표 추가 at Excel (더 구체적인 라우트를 먼저)
+router.post("/:id/upload", controller.addExcel)
+
 // 시간표 추가
 router.post("/:id", controller.add)
-
-// 시간표 추가 at Excel
-router.post("/:id/upload", controller.addExcel)
 
 // 시간표 수정
 router.put("/:id", controller.update)
