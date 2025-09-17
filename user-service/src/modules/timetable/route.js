ec396f18c438c7899be877d2bcb798265272fdf4
Jin-0-2 <jjy020815@gmail.com>
2025-09-17 17:03:00 +0900

    timetable jwt 인증 장착

--- user-service/src/modules/timetable/route.js
+++ user-service/src/modules/timetable/route.js
@@ -7,20 +7,20 @@ const authMiddleware = require("../../middleware/auth")
 
 // 시간표 전체 조회
 
-// 내 시간표 불러오기
-router.get("/:id", controller.getAll)
+// 내 시간표 불러오기  /:id >  /
+router.get("/", authMiddleware, controller.getAll)
 
-// 시간표 추가 at Excel (더 구체적인 라우트를 먼저)
-router.post("/:id/upload", controller.addExcel)
+// 시간표 추가 at Excel (더 구체적인 라우트를 먼저) /:id/upload
+router.post("/upload", authMiddleware, controller.addExcel)
 
 // 시간표 추가
-router.post("/:id", controller.add)
+router.post("/", authMiddleware, controller.add)
 
 // 시간표 수정
-router.put("/:id", controller.update)
+router.put("/", authMiddleware, controller.update)
 
 // 시간표 삭제
-router.delete("/:id", controller.delete);
+router.delete("/", authMiddleware, controller.delete);
 
 // 건물명 매핑 조회
 
