745bb28006ac7eff06f23d053d0e868cf51e7713
Jin-0-2 <jjy020815@gmail.com>
2025-09-18 16:45:22 +0900

    friends 모듈 jwt 인증 추가

--- user-service/src/modules/friends/route.js
+++ user-service/src/modules/friends/route.js
@@ -7,30 +7,30 @@ const authMiddleware = require("../../middleware/auth")
 
 
 // 친구 목록 전체 조회
-router.get("/", controller.getAll)
+router.get("/", authMiddleware, controller.getAll)
 
 // 내 친구 조회
-router.get("/myfriend/:id", controller.getMyFriend)
+router.get("/myfriend", authMiddleware, controller.getMyFriend)
 
 // 친구 추가
-router.post("/add", controller.add)
+router.post("/add", authMiddleware, controller.add)
 
 // 친구추가 보낸 요청 목록 조회
-router.get("/my_request_list/:id", controller.my_req_list)
+router.get("/my_request_list", authMiddleware, controller.my_req_list)  
 
 // 친구추가 받은 요청 목록 조회
-router.get("/request_list/:id", controller.request_list)
+router.get("/request_list", authMiddleware, controller.request_list)
 
 // 내가 보낸 친구 요청 취소
-router.post("/mistake/:id", controller.mistake);
+router.post("/mistake", authMiddleware, controller.mistake);
 
 // 친구 요청 수락
-router.post("/accept", controller.accept)
+router.post("/accept", authMiddleware, controller.accept)
 
 // 친구 요청 거절
-router.post("/reject", controller.reject)
+router.post("/reject", authMiddleware, controller.reject)
 
 // 친구 삭제
-router.delete("/delete", controller.delete)
+router.delete("/delete", authMiddleware, controller.delete)
 
 module.exports = router
