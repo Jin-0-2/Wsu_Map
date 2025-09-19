6ac3472b81e300531fda0bcedba151834fbd48ab
Jin-0-2 <jjy020815@gmail.com>
2025-09-19 17:24:34 +0900

    timetable 코드 정리

--- user-service/src/modules/timetable/controller.js
+++ user-service/src/modules/timetable/controller.js
@@ -7,31 +7,36 @@ const upload = multer();
 
 // 내 시간표 불러오기
 exports.getAll = async (req, res) => {
-    try {
-        const id = req.user.id;
+  try {
+    const id = req.user.id;
 
-        const result = await Service.getAll(id);
+    const result = await Service.getAll(id);
 
-        const response = result.rows.map(row => ({
-            title: row.title,
-            day_of_week: row.day_of_week,
-            start_time: row.start_time,
-            end_time: row.end_time,
-            building_name: row.building_name,
-            floor_number: row.floor_number,
-            room_name: row.room_name,
-            professor: row.professor,
-            color: row.color,
-            memo: row.memo,
-        }))
+    const response = result.rows.map(row => ({
+      title: row.title,
+      day_of_week: row.day_of_week,
+      start_time: row.start_time,
+      end_time: row.end_time,
+      building_name: row.building_name,
+      floor_number: row.floor_number,
+      room_name: row.room_name,
+      professor: row.professor,
+      color: row.color,
+      memo: row.memo,
+    }))   
 
-        console.log('response:', response);
+    res.status(200).json({
+      success: true,
+      response: response,
+    });
 
-        res.status(200).json(response);
     } catch (err) {
-        console.error("DB 오류:", err);
+      console.error("DB 오류:", err);
 
-        res.status(500).send("DB 오류");
+      res.status(500).json({
+        success: false,
+        message: "DB 오류"
+      });
     }
 };
 
@@ -61,7 +66,7 @@ exports.add = async (req, res) => {
         color, memo
     );
 
-    res.status(201).json({ success: true, result });
+    res.status(201).json({ success: true, data: result });
   } catch (err) {
     console.error("DB 오류:", err);
     
@@ -186,6 +191,6 @@ exports.delete = async (req, res) => {
     });
   } catch (err) {
     console.error("DB 오류:", err);
-    res.status(500).send("DB 오류");
+    res.status(500).json({ success: "DB 오류"});
   }
 };
--- user-service/src/modules/timetable/service.js
+++ user-service/src/modules/timetable/service.js
@@ -4,24 +4,25 @@ const con = require("../../core/db")
 const XLSX = require('xlsx');
 const { buildingMappings } = require('../../config/building-mappings');
 
-exports.getAll = (id) => {
-    const select_query = `
-    SELECT * FROM "timetable" where "user_id" = $1
+exports.getAll = async (id) => {
+
+  const select_query = `
+  SELECT * FROM "timetable" where "user_id" = $1
     `
 
-    const values = [id]
+  const values = [id]
 
-    return new Promise((resolve, reject) => {
-        con.query(select_query, values, (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        });
-    });
+  try {
+    const result = await con.query(select_query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
 // 시간표 추가
-exports.add = (id, title, day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo) => {
-    const query = `
+exports.add = async (id, title, day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo) => {
+  const query = `
       INSERT INTO "timetable" (
       "user_id", "title", "day_of_week", "start_time", "end_time",
       "building_name", "floor_number", "room_name", "professor",
@@ -30,62 +31,61 @@ exports.add = (id, title, day_of_week, start_time, end_time, building_name, floo
   `
   const values = [id, title, day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo === undefined ? null : memo]
 
-  return new Promise((resolve, reject) => {
-    con.query(query, values, (err, result) => {
-      if (err) return reject(err);
-      resolve(result);
-    });
-  });
+  try {
+    const result = await con.query(query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
 // 시간표 수정
-exports.update = (id, origin_title, origin_day_of_week, new_title, new_day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo) => {
-    // 쿼리 완성
-    const query = `
+exports.update = async (id, origin_title, origin_day_of_week, new_title, new_day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo) => {
+  // 쿼리 완성
+  const query = `
     UPDATE "timetable" SET "title" = $1, "day_of_week" = $2, "start_time" = $3, "end_time" = $4, "building_name" = $5, "floor_number" = $6, "room_name" = $7, "professor" = $8, "color" = $9, "memo" = $10
     WHERE "user_id" = $11 AND "title" = $12 AND "day_of_week" = $13
   `;
 
-    const values = [new_title, new_day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo, id, origin_title, origin_day_of_week]
+  const values = [new_title, new_day_of_week, start_time, end_time, building_name, floor_number, room_name, professor, color, memo, id, origin_title, origin_day_of_week]
 
-    return new Promise((resolve, reject) => {
-        con.query(query, values, (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        });
-    });
+  try {
+    const result = await con.query(query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
 // 시간표 삭제
-exports.delete = (id, title, day_of_week) => {
-    const query = `
-    DELETE FROM "timetable" WHERE "user_id" = $1 AND "title" = $2 AND "day_of_week" = $3;
-    `
+exports.delete = async(id, title, day_of_week) => {
+  const query = `
+  DELETE FROM "timetable" WHERE "user_id" = $1 AND "title" = $2 AND "day_of_week" = $3;
+  `
 
-    const values = [id, title, day_of_week]
+  const values = [id, title, day_of_week]
 
-
-    return new Promise((resolve, reject) => {
-        con.query(query, values, (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        });
-    });
+  try {
+    const result = await con.query(query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 
 // 시간표 전체 삭제
-exports.deleteAll = (id) => {
-    const query = `
-    DELETE FROM "timetable" WHERE "user_id" = $1;
-    `
-    const values = [id]
+exports.deleteAll = async (id) => { 
+  const query = `
+  DELETE FROM "timetable" WHERE "user_id" = $1;
+  `
+  const values = [id]
 
-    return new Promise((resolve, reject) => {
-        con.query(query, values, (err, result) => {
-            if (err) return reject(err);
-            resolve(result);
-        });
-    });
+  try {
+    const result = await con.query(query, values);
+    return result;
+  } catch (err) {
+    throw err;
+  }
 }
 // 엑셀 파일 파싱 (우송대학교 수강내역 형식)
 exports.parseExcelFile = async (buffer) => {
