30686bb25fba5fa559df22bf84876d88c49a6d2c
unknown <jjy020815@gmail.com>
2025-09-22 16:00:24 +0900

    커밋

--- building-service/src/app.js
+++ building-service/src/app.js
@@ -5,8 +5,8 @@ const app = express();
 const Router = require('./routes/index');
 const { initIndoorGraph, initOutdoorGraph } = require('./modules/path/service');
 const { requestLogger } = require('./core/logger');
+require('dotenv').config();
 
-const AWS_IP = `http://54.252.215.95:`
 
 app.use(express.json());
 app.use(requestLogger);
@@ -17,5 +17,5 @@ app.listen(PORT, '0.0.0.0', async () => {
   await initIndoorGraph();
   await initOutdoorGraph();
 
-  console.log(`Server running on ${AWS_IP}${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
