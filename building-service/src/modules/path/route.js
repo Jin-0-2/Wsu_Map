6501041455c059682eb13820e9d5ca9b5edaf410
jinyoung <jjy020815@gmail.com>
2025-09-25 10:35:29 +0900

    ㅋㅁ

--- building-service/src/modules/path/route.js
+++ building-service/src/modules/path/route.js
@@ -3,8 +3,8 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("./controller")
-const auth = require("../../middleware/auth")
-const admin = require("../../middleware/admin")
+const authMiddleware = require("../../middleware/auth")
+const adminMiddleware = require("../../middleware/admin")
 
 // 경로 조회 및 반환
 router.post("/", controller.getPath)
@@ -13,19 +13,19 @@ router.get("/", controller.getNodes)
 
 router.get("/edges", controller.getEdges)
 
-router.put("/", auth, admin, controller.update_node_location)
+router.put("/", authMiddleware, adminMiddleware, controller.update_node_location)
 
 // 외부
 // 건물/경로 추가
-router.post("/create", auth, admin, controller.create)
+router.post("/create", authMiddleware, adminMiddleware, controller.create)
 
 // 노드/건물 삭제
-router.delete("/", auth, admin, controller.delete);
+router.delete("/", authMiddleware, adminMiddleware, controller.delete);
 
 // 노드끼리 잇기
-router.post("/connect", auth, admin, controller.connect)
+router.post("/connect", authMiddleware, adminMiddleware, controller.connect)
 
 // 노드 연결 해제
-router.delete("/disconnect", auth, admin, controller.disconnect);
+router.delete("/disconnect", authMiddleware, adminMiddleware, controller.disconnect);
 
 module.exports = router
